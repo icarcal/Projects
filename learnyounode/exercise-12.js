056e269639f465d160dc10df48cbfaf2e92bc876
icarcal <icaro.caldeira@hotmail.com>
2015-08-05 11:07:02 -0300

    improve solution of exercise 12

--- learnyounode/exercise-12.js
+++ learnyounode/exercise-12.js
@@ -1,16 +1,22 @@
 var http = require('http');
 var url = require('url');
 
+var getResponse = function (urlIn){
+	var urlParsed = url.parse(urlIn,true);
+	var date = new Date(urlParsed.query.iso);
+
+	var routes = {
+		'/api/parsetime' : JSON.stringify({'hour':date.getHours(),'minute':date.getMinutes(),'second':date.getSeconds()}),
+		'/api/unixtime' : JSON.stringify({'unixtime':date.getTime()})
+	};
+
+	return routes[urlParsed.pathname];
+}
+
+
 var server = http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type':'application/json'})
 
-	var urlParsed = url.parse(req.url,true);
-	var date = new Date(urlParsed.query.iso);
-
-	if(urlParsed.pathname == '/api/parsetime'){
-		res.end(JSON.stringify({'hour':date.getHours(),'minute':date.getMinutes(),'second':date.getSeconds()}));
-	} else if (urlParsed.pathname == '/api/unixtime'){
-		res.end(JSON.stringify({'unixtime':date.getTime()}));
-	}
+	res.end( getResponse(req.url) );
 });
 server.listen(process.argv[2]);
